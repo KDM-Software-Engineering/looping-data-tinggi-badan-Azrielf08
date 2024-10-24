d231cc2de555734ba752c0c52e88a68224fed87f
Azrielf08 <ellovingmahen@gmail.com>
2024-10-24 14:53:58 +0700

    selesai

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-function runner() {
+//function runner() {
 
   // TULIS SCRIPT KAMU DI SINI
-
-
-
-
-
+  let tinggiBadan = [150, 155, 148, 165, 170, 168, 166]
+  let namaAtlet = ["Rizki", "Ambar", "Rada", "Tika", "Malik", "Nanang", "Azizah"]
+  console.log(`Atlet ${namaAtlet[0]} tinggi badannya ${tinggiBadan[0]} cm` )
+  console.log(`Atlet ${namaAtlet[1]} tinggi badannya ${tinggiBadan[1]} cm` )
+  console.log(`Atlet ${namaAtlet[2]} tinggi badannya ${tinggiBadan[2]} cm` )
+  console.log(`Atlet ${namaAtlet[3]} tinggi badannya ${tinggiBadan[3]} cm` )
+  console.log(`Atlet ${namaAtlet[4]} tinggi badannya ${tinggiBadan[4]} cm` )
+  console.log(`Atlet ${namaAtlet[5]} tinggi badannya ${tinggiBadan[5]} cm` )
+  console.log(`Atlet ${namaAtlet[6]} tinggi badannya ${tinggiBadan[6]} cm` )
 
   /// JANGAN UBAH SELAINNYA
-}
+//}
 
-module.exports = runner
+//module.exports = runner
