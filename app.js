44685567a00aa0e0d2503b9326d16faf92716ea7
Azrielf08 <ellovingmahen@gmail.com>
2024-10-30 10:51:18 +0700

    Pembaruan

--- app.js
+++ app.js
@@ -3,14 +3,10 @@
   // TULIS SCRIPT KAMU DI SINI
   let tinggiBadan = [150, 155, 148, 165, 170, 168, 166]
   let namaAtlet = ["Rizki", "Ambar", "Rada", "Tika", "Malik", "Nanang", "Azizah"]
-  console.log(`Atlet ${namaAtlet[0]} tinggi badannya ${tinggiBadan[0]} cm` )
-  console.log(`Atlet ${namaAtlet[1]} tinggi badannya ${tinggiBadan[1]} cm` )
-  console.log(`Atlet ${namaAtlet[2]} tinggi badannya ${tinggiBadan[2]} cm` )
-  console.log(`Atlet ${namaAtlet[3]} tinggi badannya ${tinggiBadan[3]} cm` )
-  console.log(`Atlet ${namaAtlet[4]} tinggi badannya ${tinggiBadan[4]} cm` )
-  console.log(`Atlet ${namaAtlet[5]} tinggi badannya ${tinggiBadan[5]} cm` )
-  console.log(`Atlet ${namaAtlet[6]} tinggi badannya ${tinggiBadan[6]} cm` )
+  for (let i= 0; i < 7; i++ ) {
 
+  console.log(`Atlet ${namaAtlet[i]} tinggi badannya ${tinggiBadan[i]} cm`)
+  }
   /// JANGAN UBAH SELAINNYA
 //}
 
